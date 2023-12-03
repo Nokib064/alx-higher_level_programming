c5397b209a9d2a94d407d9c92cfda4bb79d25343
ismaeel <ismaelekundayo3@gmail.com>
2023-12-03 08:02:56 -0800

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,12 +9,8 @@ if __name__ == "__main__":
         print("{} arguments:".format(s))
         for i in range(1, s + 1):
             print("{}: {}".format(i, argv[i]))
-
-
     elif s == 0:
         print("{} arguments.".format(s))
-
-
     else:
         print("{} argument:".format(s))
         print("{}: {}".format(s, argv[1]))
