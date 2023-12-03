5253e336ed515047c317d08c9a801f77972f554d
ismaeel <ismaelekundayo3@gmail.com>
2023-12-03 07:56:43 -0800

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#  Prints the number of and the list of its arguments
+if __name__ == "__main__":
+    from sys import argv
+    j = len(argv)
+    s = j - 1
+
+    if s > 1:
+        print("{} arguments:".format(s))
+        for i in range(1, s + 1):
+            print("{}: {}".format(i, argv[i]))
+
+
+    elif s == 0:
+        print("{} arguments.".format(s))
+
+
+    else:
+        print("{} argument:".format(s))
+        print("{}: {}".format(s, argv[1]))
