dd97edc38fd21d42aef18c9e7d5ac8539ccba09c
Ismaeel <ismaelekundayo3@gmail.com>
2023-12-19 15:57:56 -0800

    oop corrcted

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,11 +2,9 @@
 """Square module."""
 class Square:
     """Defines a square."""
-
     def __init__(self, size):
         """Constructor.
         Args:
             size: length of side of the square.
         """
         self.__size = size
-        
