6840e54319a3edc71624a561d348c9bd5cad429f
Ismaeel <ismaelekundayo3@gmail.com>
2023-12-19 16:02:42 -0800

    updated

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,6 @@
 
 class Square:
     """Represent a square."""
-
     def __init__(self, size=0):
         """Initialize a new Square.
         Args:
@@ -15,4 +14,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
